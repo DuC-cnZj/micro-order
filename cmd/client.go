b928416daaa4c0dc9f97282ee3058805e824e172
duc <1025434218@qq.com>
2020-11-06 17:40:44 +0800

    wip

--- cmd/client.go
+++ cmd/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"github.com/DuC-cnZj/goods_proto"
 	order "github.com/DuC-cnZj/micro-order/protos"
 	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc"
@@ -10,14 +11,19 @@ import (
 
 func main() {
 	dial, err := grpc.Dial("localhost:6666", grpc.WithInsecure())
+	goodsDial, err := grpc.Dial("localhost:9999", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("grpc dial error %v", err)
 	}
 
 	client := order.NewOrderClient(dial)
+	goodsClient := goods_proto.NewBBQClient(goodsDial)
 	list, err := client.List(context.Background(), &empty.Empty{})
 	if err != nil {
 		log.Fatalf("err call order svc list: %v", err)
 	}
-	log.Println(list)
+	eat, _ := goodsClient.Eat(context.Background(), &goods_proto.Request{
+		Name: "from order call",
+	})
+	log.Println(list, eat)
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,10 @@ module github.com/DuC-cnZj/micro-order
 go 1.15
 
 require (
+	github.com/DuC-cnZj/goods_proto v0.0.0-20201106092255-0a0f4eb22d12
 	github.com/golang/protobuf v1.4.3
 	golang.org/x/net v0.0.0-20201031054903-ff519b6c9102 // indirect
-	golang.org/x/sys v0.0.0-20201101102859-da207088b7d1 // indirect
+	golang.org/x/sys v0.0.0-20201106081118-db71ae66460a // indirect
 	golang.org/x/text v0.3.4 // indirect
 	google.golang.org/genproto v0.0.0-20201105153401-9d023cd09d72 // indirect
 	google.golang.org/grpc v1.33.1
